540dc6ead43e6be35aec9a96db0603d9653e8eea
Vyacheslav <tvo0@ya.ru>
2021-07-02 16:34:19 +0500

    Адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
+    $('.mobile-bar__menu-button').on('click', function () {
+        $('header.header').toggleClass('show-mobile-menu')
+    });
 
+    if($(document).outerWidth() <= 1000) {
+        $('.main-menu li.has-sub-menu').on('click', function (e) {
+            e.preventDefault();
+            $(this).toggleClass('show');
+
+            if($(this).hasClass('show')) {
+                $(this).find('.sub-menu').slideDown(200);
+            }
+            else {
+                $(this).find('.sub-menu').slideUp(200);
+            }
+        });
+    }
 })
